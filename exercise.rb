8bd66b49b6d6af9d2de2ce96769ef851c4c1e1b4
Noe Lomeli <nolomeli@csumb.edu>
2019-01-31 21:00:35 -0800

    Add comments to first function

--- exercise.rb
+++ exercise.rb
@@ -8,20 +8,20 @@ class Exercise
     
     arr = Array.new
     char = ""
-    for s in str.split(" ")
-      if s[-1] =~ /[^a-zA-Z]/
-        char = s[-1]
+    for s in str.split(" ") # split string using spaces as delimiter and iterate through each word
+      if s[-1] =~ /[^a-zA-Z]/ # if the last character of string is not a letter we want to save it
+        char = s[-1]      
       end
-      if s.length > 4 && s =~ /[A-Z]/
+      if s.length > 4 && s =~ /[A-Z]/ # check if length of word is greater than four and starts with a capital letter
         s = "Marklar" + char
       
-      elsif  s.length > 4
+      elsif  s.length > 4 
         s = "marklar" + char
       end
-      arr.push(s)
+      arr.push(s) # add every word to array
       
     end
-    p arr.join(" ")  
+    p arr.join(" ")  # make array a string and return
 end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -29,7 +29,7 @@ end
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method  
-  print "sum: " , sum
+    # TODO: Implement this method
   end
+
 end
