b95ef78e2194fe1737f195f3fb2ba267b4ca6d40
Noe Lomeli <nolomeli@csumb.edu>
2019-01-31 20:51:24 -0800

    Completed first function

--- exercise.rb
+++ exercise.rb
@@ -5,14 +5,31 @@ class Exercise
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
     # TODO: Implement this method
-  end
+    
+    arr = Array.new
+    char = ""
+    for s in str.split(" ")
+      if s[-1] =~ /[^a-zA-Z]/
+        char = s[-1]
+      end
+      if s.length > 4 && s =~ /[A-Z]/
+        s = "Marklar" + char
+      
+      elsif  s.length > 4
+        s = "marklar" + char
+      end
+      arr.push(s)
+      
+    end
+    p arr.join(" ")  
+end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
   # the "nth" term in the sequence
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
+    # TODO: Implement this method  
+  print "sum: " , sum
   end
-
 end
